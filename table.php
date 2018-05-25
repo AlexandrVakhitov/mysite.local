d950a1750e382419707838a0e92600084cf27970
lesterEkb <vakhitov.projects@yandex.ru>
2018-05-25 21:57:50 +0500

    Изминение 1

--- table.php
+++ table.php
@@ -1,24 +1,9 @@
 <?
  require_once "include/libs.php";
-?>
 
-<!DOCTYPE html>
-<html>
+ require_once "include/header.php";
 
-<head>
-  <title>Таблица умножения</title>
-  <meta charset="utf-8" />
-  <link rel="stylesheet" href="style.css" />
-</head>
-
-<body>
-
-  <div id="header">
-    <!-- Верхняя часть страницы -->
-    <img src="logo.gif" width="187" height="29" alt="Наш логотип" class="logo" />
-    <span class="slogan">приходите к нам учиться</span>
-    <!-- Верхняя часть страницы -->
-  </div>
+ ?>
 
   <div id="content">
     <!-- Заголовок -->
